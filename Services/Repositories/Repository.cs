ca93124d8587609f432aed6682fb7a2bb35b608a
LXX1029 <duan20180126@163.com>
2020-11-28 22:03:31 +0800

    submit code

--- Services/Repositories/Repository.cs
+++ Services/Repositories/Repository.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.UI.WebControls;
 using Services.EFCodeFirst;
 
 namespace Services.Repositories
